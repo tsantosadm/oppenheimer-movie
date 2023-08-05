537945a7820d2886916e9d568f6829bacf5d528b
Talisson <talisson.santos@apegro.com>
2023-08-04 23:01:37 -0300

    changes buttons

---
+++ main.js
@@ -0,0 +1,8 @@
+const modalContainer = document.getElementById("modalContainer");
+const openModal = document.getElementById("openModal");
+const youtubeIframe = document.getElementById("youtubeIframe");
+
+openModal.addEventListener("click", function(){
+   modalContainer.style.display = 'block';
+})
+
